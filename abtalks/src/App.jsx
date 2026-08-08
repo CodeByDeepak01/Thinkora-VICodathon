22bf37a949963ade28fbafe31eb5be9031aa5360
Deepak <dpal789557@gmail.com>
2026-08-08 23:55:26 +0530

    Fix production build

--- abtalks/src/App.jsx
+++ abtalks/src/App.jsx
@@ -3,7 +3,7 @@ import Layout from "./components/layout/Layout";
 import LandingPage from "./pages/LandingPage";
 import DashboardPage from "./pages/DashboardPage";
 import ChallengeDayPage from "./pages/ChallengeDayPage";
-import LeaderboardPage from "./pages/LeaderboardPage";
+import LeaderBoardPage from "./pages/LeaderBoardPage";
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/day/12" element={<ChallengeDayPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/leaderboard" element={<LeaderBoardPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
