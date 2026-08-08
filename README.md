7161af85bc1f88d3e0a1a9bc7a44acbf9261771e
Deepak <dpal789557@gmail.com>
2026-08-08 22:58:45 +0530

    Move project into abtalks folder

--- README.md
+++
@@ -1,2 +0,0 @@
-# Thinkora-VICodathon
-Official repository for our ABTalks ViCodathon 2026 submission.
--- src/components/ui/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Button } from "./Button";
-export { default as Card } from "./Card";
-export { default as ProgressBar } from "./ProgressBar";
-export { default as Badge } from "./Badge";
-export { default as Input } from "./Input";
-export { default as EmptyState } from "./EmptyState";
-export { default as ErrorState } from "./ErrorState";
-export { default as LoadingSkeleton } from "./LoadingSkeleton";
-export { default as ValidationMessage } from "./ValidationMessage";
